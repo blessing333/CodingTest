a73d04cd943bb694cf05b0a082642b945a7f9717
blessing333 <blessing_333@naver.com>
2020-09-16 02:55:07 +0900

    좋은 단어
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Baekjoon/Chapter2/3986_좋은단어.cpp
+++ Baekjoon/Chapter2/3986_좋은단어.cpp
@@ -1,27 +1,23 @@
-/*
-    1.스택에 하나씩 넣어서 같은 알파벳이면 제거
-*/
 #include<bits/stdc++.h>
 using namespace std;
-
-int n;
-int cnt;
+int n,cnt;
 string str;
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
     cin >> n;
     for(int i=0;i<n;i++){
         stack<char> s;
         cin >> str;
-        for(auto c: str){
-            if(!s.empty() && s.top()==c)
+        for(int i=0;i<str.size();i++){
+            if(!s.empty()&&s.top()==str[i]){
                 s.pop();
+            }
             else
-                s.push(c);
+                s.push(str[i]);
         }
-    if(s.empty())
+
+        if(s.empty())
             cnt++;
-    
     }
     cout << cnt;
 }
