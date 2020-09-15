56f2e99c30e97c0e4ee541aa7fd15bb859d12418
blessing333 <blessing_333@naver.com>
2020-09-16 02:53:44 +0900

    그림
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Baekjoon/Chapter2/1926_그림.cpp
+++ Baekjoon/Chapter2/1926_그림.cpp
@@ -1,53 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int n,m,k,cnt,area,tmp;
+int n,m;
 int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-int board[505][505];
-int visit[505][505];
+int dy[4]={0,-1,0,1};
+int result,cnt;
 queue<pair<int,int>> q;
 
-void bfs(int x,int y){
-    tmp=1;
+int board[500][500];
+int visit[500][500];
+void bfs(int x , int y){
+    int area=1;
     visit[x][y]=1;
     q.push({x,y});
     while(!q.empty()){
-        pair<int,int> cur=q.front();q.pop();
+        pair<int, int > cur=q.front();q.pop();
         for(int i=0;i<4;i++){
             int nx=cur.first+dx[i];
             int ny=cur.second+dy[i];
-            if(nx>=n||nx<0||ny>=m||ny<0)continue;
-            if(visit[nx][ny]==0 && board[nx][ny]==1){
-                visit[nx][ny]=1;
-                q.push({nx,ny});
-                tmp++;
-            }
+            if(nx>=n ||nx<0 ||ny>=m||ny<0)continue;
+            if(visit[nx][ny]||!board[nx][ny]) continue;
+            visit[nx][ny]=1;
+            q.push({nx,ny});
+            area=area+1;
         }
     }
-    if(area<tmp)
-        area=tmp;
+    result=max(result,area);
 }
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
     cin >> n >> m;
-
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cin >> k;
-            board[i][j]=k;
+            cin >> board[i][j];
         }
     }
-    
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(visit[i][j]==0 && board[i][j]==1){
+            if(!visit[i][j] && board[i][j]){
                 bfs(i,j);
-                cnt++;
+                cnt ++;
             }
         }
     }
-
-    cout << cnt << "\n";
-    cout << area;
+    cout << cnt<<"\n";
+    cout << result;
 }
