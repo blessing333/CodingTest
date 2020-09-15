ce2efc4d5ca8508e7e99fdfa71598474c8bf61a0
blessing333 <blessing_333@naver.com>
2020-09-16 02:57:14 +0900

    숨바꼭질2
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Baekjoon/Chapter2/12851_숨바꼭질2.cpp
+++ Baekjoon/Chapter2/12851_숨바꼭질2.cpp
@@ -1,34 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int n,k;
-int dist[100001];
-int visit[100001];
-int cnt[100001];
+int visit[100005];
+int dist[100005];
+int cnt[100005];
 queue<int> q;
+
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin >> n >> k;
-    visit[n]=1;
-    cnt[n]=1;
     q.push(n);
-
+    cnt[n]=1;
+    visit[n]=true;
     while(!q.empty()){
-        int cur=q.front();q.pop();
-        for(int next : {cur-1,cur+1,cur*2}){
-            if(next>=0 && next <=100000){
-                if(visit[next]==0){
-                    visit[next]=1;
-                    dist[next]=dist[cur]+1;
-                    q.push(next);
-                    cnt[next]=cnt[cur];
-                }
-                else if(dist[next]==dist[cur]+1){
-                    cnt[next]+=cnt[cur];
-                }
+        int cur=q.front();
+        q.pop();
+        for(int nx :{cur-1,cur+1,cur*2}){
+            if(nx>100000 || nx<0)continue;
+            if(!visit[nx]){
+                visit[nx]=true;
+                dist[nx]=dist[cur]+1;
+                q.push(nx);
+                cnt[nx]=cnt[cur];
+            }
+            else if(dist[nx]==dist[cur]+1){
+                cnt[nx]+=cnt[cur];
             }
         }
     }
-    
-    cout << dist[k] << "\n";
+    cout << dist[k] <<"\n";
     cout << cnt[k];
 }
