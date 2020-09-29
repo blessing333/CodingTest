03a053c0375451b2117419005e27a979fee090d3
blessing333 <blessing333@naver.com>
2020-09-29 15:57:07 +0900

    파이프 옮기기 dfs로 수정
    Signed-off-by: blessing333 <blessing333@naver.com>

--- Baekjoon/Chapter5/17070_파이프옮기기.cpp
+++ Baekjoon/Chapter5/17070_파이프옮기기.cpp
@@ -1,88 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,board[16][16],cnt;
-int dx[3][3]={
-    {0,1,-1},
-    {1,1,-1},
-    {0,1,1}
-};
-int dy[3][3]={
-    {1,1,-1},
-    {0,1,-1},
-    {1,0,1}
-};
-int dstate[3][3]={
-    {0,2,-1},
-    {0,1,-1},
-    {-2,-1,0}
-};
+int dx[]={0,1,1},dy[]={1,0,1};
 
-struct pipe{
-    int x;
-    int y;
-    int state; //0:가로     1:세로      2:대각선
-};
-queue<pipe> q;
-void bfs(){
-    pipe start;
-    start.x=0;start.y=1;start.state=0;
-    q.push(start);
+void dfs(int x,int y,int z){
+    if(x==n-1 && y==n-1){
+        cnt++;
+        return;
+    }
 
-    while(!q.empty()){
-        pipe cur=q.front();q.pop();
-        if(cur.x==n-1 && cur.y ==n-1){
-            cnt++;
-            continue;
-        }
-       int dir;
-        if(cur.state==0){
-            dir=2;
-        }
-        else if(cur.state==1){
-            dir=2;
-        }
-        else if(cur.state==2){
-            dir=3;
-        }
-
-        for(int i=0;i<dir;i++){
-            int nx=cur.x+dx[cur.state][i];
-            int ny=cur.y+dy[cur.state][i];
-            int nstate=cur.state+dstate[cur.state][i];
-
-            if(nx>=n || ny >=n || nx<0 || ny <0) continue;
-            // if(nstate==0 && !board[nx][ny]){
-            //     pipe npipe;
-            //     npipe.x=nx; npipe.y=ny; npipe.state=nstate;
-            //     q.push(npipe);
-            // }
-            // else if(nstate==1 && !board[nx][ny]){
-            //     pipe npipe;
-            //     npipe.x=nx; npipe.y=ny; npipe.state=nstate;
-            //     q.push(npipe);
-            // }
-            // else if(nstate==2 && !board[nx][ny] &&!board[nx-1][ny]&& !board[nx][ny-1]){
-            //     pipe npipe;
-            //     npipe.x=nx; npipe.y=ny; npipe.state=nstate;
-            //     q.push(npipe);
-            // }
-            if((nstate==1 || nstate==0) && !board[nx][ny]){
-                //만약 현재 상태가 0,1이고 nx ny이 1이 아니라면
-                pipe npipe;
-                npipe.x=nx;npipe.y=ny;npipe.state=nstate;
-                q.push(npipe);
-            }
-            else if(nstate==2 && !board[nx][ny] && !board[nx-1][ny] && !board[nx][ny-1]){
-                //만약 현재 상태가 2이고 nx ny와 그 왼쪽, 위가 1이 아니라면
-                pipe npipe;
-                npipe.x=nx;npipe.y=ny;npipe.state=nstate;
-                q.push(npipe);
-            }
-
-        }
+    for(int i=0;i<3;i++){
+        if (i + z==1)continue;
+		int nx = x + dx[i], ny = y + dy[i];
+		if (nx >= n || ny >= n || board[nx][ny])continue;
+		if (i == 2 && (board[x + 1][y] || board[x][y + 1]))continue;
+        dfs(nx,ny,i);
     }
 }
-
 int main(){
     cin >> n;
     for(int i=0;i<n;i++){
@@ -90,6 +24,6 @@ int main(){
             cin >> board[i][j];
         }
     }
-    bfs();
+    dfs(0,1,0);
     cout << cnt;
 }
