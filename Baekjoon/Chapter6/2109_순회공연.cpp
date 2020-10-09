24ef318f1eda24a8222b525b54e5b3d19af43eed
blessing333 <blessing_333@naver.com>
2020-10-09 15:41:50 +0900

    순회 강연
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Baekjoon/Chapter6/2109_순회공연.cpp
+++ Baekjoon/Chapter6/2109_순회공연.cpp
@@ -1,37 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,d,p,sum;
-int arr[100001];
 vector<pair<int,int>> v;
+priority_queue<int,vector<int>,greater<int>> pq;
 
 int main(){
-    scanf("%d",&n);
-    int de=0;
-    
+  
+    cin >> n;
     for(int i=0;i<n;i++){
         //second = pay, first=day
-        int p,d;scanf("%d%d",&p,&d);
-        arr[d-1]++;
+        cin >> p >> d;
         v.push_back({d,p});
-        //cout << de<<". first = "<<v[de].first << "second = " <<v[de].second<<"\n";
-        //de++;
     }
-    
     sort(v.begin(),v.end());
-    
-    int index=0;
     for(int i=0;i<v.size();i++){
-        cout << i;
-        if(index>i) continue;
-        int money=0;
-        for(int j=i;j<i+arr[v[i].first-1];j++){
-            money=max(money,v[j].second);
-            cout << "j : " <<j<<"\n";
-            cout << "arr[] : "<<j+arr[v[i].first-1] <<"\n";
+        sum += v[i].second;
+        pq.push(v[i].second);
+        if(v[i].first<pq.size()){
+            sum-=pq.top();pq.pop();
         }
-        sum+=money;
-        printf("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
-        index=i+arr[v[i].first-1];
     }
+    
     cout <<sum;
 }
