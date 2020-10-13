9819be631ef57ec3d9f7f697e9f55f1195dc8977
blessing333 <blessing333@naver.com>
2020-10-13 23:14:09 +0900

    분노 유발자
    Signed-off-by: blessing333 <blessing333@naver.com>

--- Inflearn/Chapter1_Simulation/19.cpp
+++ Inflearn/Chapter1_Simulation/19.cpp
@@ -7,17 +7,10 @@ int main(){
     for(int i=0;i<n;i++){
         int a; cin >> a; v.push_back(a);
     }
-
-    for(int i=0;i<n-1;i++){
-        bool flag=false;
-        int cur=v[i];
-        for(int j=i+1;j<n;j++){
-            if(cur<=v[j]){
-                flag=true;
-                break;
-            }
-        }
-        if(!flag){
+    int max=v[n-1];
+    for(int i=n-2;i>=0;i--){
+        if(v[i]>max){
+            max=v[i];
             cnt++;
         }
     }
