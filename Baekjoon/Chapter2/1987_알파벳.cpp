fb465068a52776e4340d9efaf36ab7205a8ccaed
blessing333 <blessing_333@naver.com>
2020-11-30 18:44:56 +0900

    [Bekjoon] 알파벳
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Baekjoon/Chapter2/1987_알파벳.cpp
+++ Baekjoon/Chapter2/1987_알파벳.cpp
@@ -1,37 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-int r,c,result;
-bool flag=false;
+int r,c,ret;
 char board[20][20];
-bool visit[20][20];
-int arr[26];
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-stack<char> s;
-void dfs(int x,int y, int cnt){
+bool check[26];
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
+void dfs(int x, int y,int cnt){
+    ret=max(cnt,ret);
     for(int i=0;i<4;i++){
         int nx=x+dx[i];
         int ny=y+dy[i];
-        if(nx>=r||nx<0||ny>=c||ny<0) continue;
-        
-        if(arr[board[nx][ny]-'A']){
-            continue;
-        }
-        arr[board[nx][ny]-'A']=1;
-        result=max(result,cnt);
-        dfs(nx,ny,cnt+1);
 
+        if(nx >= r || nx<0 || ny>=c || ny<0) continue;
+        int k=(int)board[nx][ny]-'A';
+        if(check[k]) continue;
+        check[k]=true;
+        dfs(nx,ny,cnt+1);
+        check[k]=false;
     }
+    
 }
 int main(){
-    cin >> r >> c;
+    scanf("%d %d",&r,&c);
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
-            cin >> board[i][j];
+            scanf(" %1c",&board[i][j]);
         }
     }
-    arr[board[0][0]-'A']=1;
+    check[(int)board[0][0]-'A']=true;
     dfs(0,0,1);
-    cout<<result;
-    
+    printf("%d",ret);
+
 }
