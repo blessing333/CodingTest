41d1728e070800568be0ef8ae3d8e281e6674062
blessing333 <blessing_333@naver.com>
2021-01-20 15:30:29 +0900

    [Programmers] 뉴스 클러스터링(2018 카카오 블라인드 채용)
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Programmers/level2/뉴스 클러스터링.cpp
+++ Programmers/level2/뉴스 클러스터링.cpp
@@ -1,6 +1,4 @@
 #include<bits/stdc++.h>
-
-
 using namespace std;
 void createToken(string str, vector<string> &set1){
     string token;
@@ -8,22 +6,20 @@ void createToken(string str, vector<string> &set1){
     while(idx<str.size()){
         bool check=true;
         string token=str.substr(idx,2);
-        idx+=2;
+        idx++;
         if(token.size()<2) break;
         for(int i=0;i<token.size();i++){
             char it = token[i];
             if((!(it >='a' && it<='z') && !(it >='A' && it<='Z'))) check=false;
             else if(it >='A' && it<='Z') token[i]+=32;
         }
-        if(check){
-            set1.push_back(token);
-        }
+        if(check) set1.push_back(token);
     }
-
-
 }
 int solution(string str1, string str2) {
     long long ret=0;
+    double jakad;
+    int intersectionCount=0,unionCount=0;
     vector<string> set1,set2,intersectionSet,unionSet;
     string token;
     int idx=0;
@@ -38,21 +34,19 @@ int solution(string str1, string str2) {
 
     set_intersection(set1.begin(),set1.end(),set2.begin(),set2.end(),intersectionSet.begin());
     set_union(set1.begin(),set1.end(),set2.begin(),set2.end(),unionSet.begin());
+    
+    for(auto it : unionSet) if(it!="") unionCount++;
+    for(auto it : intersectionSet) if(it!="") intersectionCount++;
 
-    if(unionSet.size()==0)
-        return 0;
-    else 
-        return intersectionSet.size()/
-    
-    for(auto it : set1){
-        cout << it << "\n";
-    }
-    cout << "\n";
-    
-    // for(auto it : set2){
-    //     cout << it << "\n";
-    // }
-    return 0;
+    if(unionCount == 0 && intersectionCount==0)
+        jakad = 1;
+    else if(unionCount==0)
+        jakad = 0;
+    else
+        jakad=(double)intersectionCount/ (double)unionCount;
+        
+    ret=jakad*65536;
+    return ret;
 }
 
 int main(){
