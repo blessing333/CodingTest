77ff5f45eaba1b999c91c1d49012d6e6ecdf88c4
blessing333 <blessing_333@naver.com>
2021-02-05 17:16:09 +0900

    [Programmers] 가장 큰 수
    Signed-off-by: blessing333 <blessing_333@naver.com>

--- Programmers/level2/가장 큰 수.cpp
+++ Programmers/level2/가장 큰 수.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//사전순으로 뒤에 나오는게 먼저 나오면 가장 큰 수
-// 더 큰게 늦게 나온다
+bool cmp(const string &a , const string &b){
+    return a+b > b+a;
+}
 string solution(vector<int> numbers) {
     string answer = "";
     vector<string> v;
     for(int it : numbers){
         v.push_back(to_string(it));
     }
-    sort(v.begin(),v.end(),greater<>());
+    sort(v.begin(),v.end(),cmp);
     for(string it : v){
         answer+=it;
     }
-    
+
+    if(answer[0] == '0')
+        answer= "0";
     return answer;
 }
