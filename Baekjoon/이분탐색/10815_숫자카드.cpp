c384aa61c721c8044d5995576b54f0a0b2815e1e
blessing333 <blessing_333@naver.com>
2021-08-03 16:33:56 +0900

    [Baekjoon] 숫자 카드
    Signed-off-by: blessing333 <blessing_333@naver.com>

---
+++ Baekjoon/이분탐색/10815_숫자카드.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n;
+    vector<int> v;
+    vector<int> v2;
+    for(int i=0;i<n;i++){
+        int a; cin >> a;
+        v.push_back(a);
+    }
+    cin >> m;
+    for(int i=0;i<m;i++){
+        int a; cin >> a;
+        v2.push_back(a);
+    }
+    sort(v.begin(),v.end());
+    for(int key : v2){
+        bool result = binary_search(v.begin(),v.end(),key);
+        cout << result <<" ";
+    }
+}
